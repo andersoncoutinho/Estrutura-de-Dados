415b53ae2819cb524c13349c6214403c15dc5a5d
Gabriel-Alexandre <biellalex86@gmail.com>
2021-09-20 08:22:29 -0300

    refactor(VetorDinamico): Refatorando código de teste
    Antes: Teste com entrada de dados
    
     Agora: Teste com saída de dados

--- Listas/VetorDinamico/main.c
+++ Listas/VetorDinamico/main.c
@@ -3,93 +3,50 @@
 
 // Código de teste:
 
+void imprime_vetor(VetorDinamico *vetor) {
+    int tamanhoVetor;
+    float aux;
+
+    tamanhoVetor = tamanho_vetor(vetor);
+    printf("Tamanho: %d\n", tamanhoVetor);
+    for(int i = 0; i < tamanhoVetor; i++) {
+        acessa_vetor(vetor, i, &aux);
+        printf("%.2f ", aux);
+    }
+    printf("\n\n");
+}
+
 int main(void) {
 
     VetorDinamico *vetor;
-    // Vetor dinâmico.
-    float inserir;
-    // Valor a ser inserido.
-    float aux;
-    int aux2;
-    // Variáveis auxiliares de acesso.
-    int n;
-    // Dimensão.
-    int op_01, op_02;
-    // Variáveis de opção.
-    int tamanho_vetorV;
-    // Tamanho do vetor.
 
-    printf("Deseja um vetor de que dimensao: ");
-    scanf("%d", &n);
+    printf("Criando vetor: \n\n");
+    vetor = criaVetor(5);
+    imprime_vetor(vetor);
 
-    vetor = criaVetor(n);
+    printf("Inserindo dados: \n\n");
+    insere_vetor(vetor, 1);
+    insere_vetor(vetor, 2);
+    insere_vetor(vetor, 3);
+    insere_vetor(vetor, 4);
+    imprime_vetor(vetor);
 
-    while(1) {
-        printf("Escolha uma opcao: \n\n1- Inserir um valor\n2- Remover um valor\n3- Mostrar vetor + tamanho\n4- Redefinir tamanho da dimensao de realocacao\n5- Sair\n\n");
-        scanf("%d", &op_02);
-        printf("\n");
+    printf("Removendo ultimo elemento: \n\n");
+    remove_vetor(vetor);
+    imprime_vetor(vetor);
 
-        if(op_02 == 5)
-            break;
-        
-        if(op_02 == 1) {
-            printf("Digite um valor: ");
-            scanf("%f", &inserir);
+    printf("Usando a dimensao de realocao padrao, 10...\n");
+    printf("Inserindo novo elemento e aumentando vetor: \n\n");
+    insere_vetor(vetor, 5);
+    imprime_vetor(vetor);
 
-            aux2 = insere_vetor(vetor, inserir);
-
-            if(aux2 == 1) {
-                printf("Inserido com sucesso!\n\n");
-            }else {
-                printf("Falha ao inserir elemento!\n\n");
-            }
-        }
-
-        if(op_02 == 2) {
-            aux2 = remove_vetor(vetor);
-
-            if(aux2 == 1) {
-                printf("Ultimo valor removido com sucesso!\n\n");
-            }else {
-                printf("Falha ao remover ultimo elemento!\n\n");
-            }
-        }
-
-        if(op_02 == 3) {
-            tamanho_vetorV = tamanho_vetor(vetor);
-
-
-            if(tamanho_vetorV == -1) {
-                printf("Falha ao calcular tamanho!\n");
-            }else {
-                printf("Tamanho: %d\n\n", tamanho_vetorV);
-            }
-
-            for(int i = 0; i< tamanho_vetorV; i++) {
-                aux2 = acessa_vetor(vetor, i, &aux);
-
-                if(aux2 == 1) {
-                    printf("%.2f \n", aux);
-                }else {
-                    printf("Error\n");
-                }
-            }
-            printf("\n");
-        }
-
-        if(op_02 == 4) {
-            printf("Digite a nova dimensao de realocacao: ");
-            scanf("%d", &n);
-
-            aux2 = redefinir_dim_rlc(vetor, n);
-
-            if(aux2 == 1) {
-                printf("Dimensao de realocacao redefinida com sucesso!\n\n");
-            }else {
-                printf("Nao foi possivel redefinir a dimesao de realocacao do vetor!\n\n");
-            }
-        }
+    printf("Redefinindo dimensao de realocao para 4...\n");
+    printf("Inserindo elementos ate novo aumento do vetor: \n\n");
+    redefinir_dim_rlc(vetor, 4);
+    for(int i = 6; i < 16; i++) {
+        insere_vetor(vetor, i);
     }
+    imprime_vetor(vetor);
 
     libera_vetor(vetor);
 
